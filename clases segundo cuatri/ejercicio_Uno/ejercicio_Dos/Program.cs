87f239565269f3f82b609da833c756f9882a2786
lucianagonzalez21 <gonzalezlucianagrisel@gmail.com>
2022-08-22 22:12:44 -0300

    ejercicio dos

---
+++ clases segundo cuatri/ejercicio_Uno/ejercicio_Dos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio_Dos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            Console.WriteLine("Ingrese un numero:");
+
+            if (double.TryParse(Console.ReadLine(), out numero) && numero > 0)
+            {
+                double cuadrado = Math.Pow(numero, 2);
+                double cubo = Math.Pow(numero, 3);
+
+                Console.WriteLine($"Numero: {numero} Cuadrado: {cuadrado} Cubo: {cubo}");
+
+            }
+            else
+            {
+                Console.WriteLine("Dato no valido");
+            }
+        }
+    }
+}
