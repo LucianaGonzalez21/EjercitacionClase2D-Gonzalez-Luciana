ee9064891e505c0f9ad655a9c116560ae60c4da6
lucianagonzalez21 <gonzalezlucianagrisel@gmail.com>
2022-08-27 20:34:41 -0300

    ejercicio 4

---
+++ clases segundo cuatri/ejercicio_Uno/ejercicio_cuatro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ejercicio_cuatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadDivisores = 0;
+
+            for(int i=2; i<=7; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        cantidadDivisores++;
+                    }
+                }
+
+                if(cantidadDivisores == 2)
+                {
+                    double numeroPerfecto = Math.Pow(2, i - 1) * (Math.Pow(2, i)- 1);
+                    Console.WriteLine(numeroPerfecto);
+                }
+
+                cantidadDivisores = 0;
+            }
+        }
+    }
+}
